502dbdd9589a3206c3e67cd796f109da5c3828d6
yntymakdev <kubanychevyntymak21@gmail.com>
2025-06-17 22:20:18 +0600

    as

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -16,8 +16,15 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title=fake()->sentence();
         return [
-            "title"=> $this->faker->sentence,
+            "images"=> fake()->imageUrl(),
+            "title"=> $title,
+            "slug"=> \Illuminate\Support\Str::slug($title),
+            "content"=> fake()->paragraph(5),
+            "category_id"=> Category::inRandomOrder()->first()->id,
+            "user_id"=> 1,
+            "published_at"=>fake()->optional()->dateTime(),
         ];
     }
 }
