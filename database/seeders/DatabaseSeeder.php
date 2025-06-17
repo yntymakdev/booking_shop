f8a488b96f7f0b677550080f5a0c1a58758aa2ef
yntymakdev <kubanychevyntymak21@gmail.com>
2025-06-17 22:43:07 +0600

    as

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -28,6 +29,7 @@ class DatabaseSeeder extends Seeder
         Category::create([
             'name'=> $category,
             ]);
+            Post::factory(100)->create();
     }
 }
 }
